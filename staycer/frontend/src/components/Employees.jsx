eea16af159586d4033118bcac027615687acef81
Haris Tanvir <haristanvir@hotmail.com>
2020-09-08 20:23:23 -0400

    adding axios

---
+++ staycer/frontend/src/components/Employees.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Employees extends Component {
+    state = {  }
+
+    componentDidMount() {
+        
+    }
+    
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Employees;
