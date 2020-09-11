64402e0929edf7dbe079df40249b7935a8856c19
Haris Tanvir <haristanvir@hotmail.com>
2020-09-10 21:52:31 -0400

    package files

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,28 @@
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            }
-        ]
-    }
-}
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: true,
+              sourceMap: true,
+              localIdentName: "[path]___[name]__[local]___[hash:base64:5]",
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
