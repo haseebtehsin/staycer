89594d0e3919a61ddad65d33f55a39989faa7365
Haseeb <haseebtehsin@yahoo.com>
2020-09-19 14:31:20 -0230

    Updated Navbar

--- staycer/frontend/src/components/TopNavBar/TopNavBar.jsx
+++ staycer/frontend/src/components/TopNavBar/TopNavBar.jsx
@@ -62,6 +62,33 @@ class TopNavBar extends Component {
               </NavLink>
             </li>
           </ul>
+          <ul class="navbar-nav my-2 my-lg-0">
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a className="dropdown-item" href="#">
+                  Action
+                </a>
+                <a className="dropdown-item" href="#">
+                  Another action
+                </a>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </div>
+            </li>
+          </ul>
         </div>
       </nav>
     );
